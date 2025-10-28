aaffb52403a72166a362f7dd54223e39b350d4f4
Luke Miller <88352100+LA-Miller@users.noreply.github.com>
2025-10-28 13:59:39 -0400

    initial commit

---
+++ profile.js
@@ -0,0 +1,10 @@
+
+const setUp = () => {
+    $("#name").text("Luke Miller");
+    $("#major").text("Applied Computer Science");
+    $("#year").text("Senior");
+
+    $("h4").html("<i>Indiana</i>");
+};
+
+$(document).ready(setUp);
